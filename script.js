40f9032a551315bef050aff385a1f4b3627a6d19
Hang886 <163233305+Hang886@users.noreply.github.com>
2025-03-07 19:34:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+new Vue({
+    el: '#app',
+    data() {
+      return {
+        questions: [
+          {
+            question: "你是谁",
+            options: ["好人", "坏人", "钝角"],
+            correct: 1,
+          },
+          {
+            question: "你喜欢谁",
+            options: ["鸡块", "汉堡", "钝角"],
+            correct: 2,
+            isFinal: true, // ���Ϊ���һ������
+          },
+        ],
+        currentIndex: 0,
+        showResult: false,
+        resultGif: "",
+      };
+    },
+    computed: {
+      currentQuestion() {
+        return this.questions[this.currentIndex];
+      },
+    },
+    methods: {
+      handleAnswer(selectedIndex) {
+        if (selectedIndex === this.currentQuestion.correct) {
+          if (this.currentQuestion.isFinal) {
+            this.showResult = true;
+            this.resultGif = "https://media.giphy.com/media/3o7abKhOpu0NwenH3O/giphy.gif"; // ���Ķ�ͼ
+          } else {
+            this.currentIndex++;
+          }
+        } else if (this.currentQuestion.isFinal) {
+          this.showResult = true;
+          this.resultGif = "https://media.giphy.com/media/l0HlHFRbmaZtBRhXG/giphy.gif"; // ������ͼ
+        } else {
+          this.currentIndex++;
+        }
+      },
+      resetQuiz() {
+        this.currentIndex = 0;
+        this.showResult = false;
+      },
+    },
+  });
